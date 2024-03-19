eabd4e562ab3cf806a2ade21e58d721694abc25e
Wolf-dev-21 <wolf.of.art21@gmail.com>
2024-03-19 16:38:36 -0300

    inserts

---
+++ Inserts.sql
@@ -0,0 +1,10 @@
+#Turma
+insert into tbl_turma (cod_turma, nome, sigla)
+values (1, 'fundamental', 'A');
+
+insert into tbl_turma (cod_turma, nome, sigla)
+values (2, 'medio', 'B');
+
+#Alunos
+insert into tbl_aluno (cod_aluno, nome, cpf, rg, telefone_aluno)
+values (2, 'medio', 'B') 
